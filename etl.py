3421646d265465b7bb6ce6d19ae47aace1d0582b
sdjhen <114769817+sdjhen@users.noreply.github.com>
2025-11-26 16:41:09 +0000

    created transform function

--- etl.py
+++ etl.py
@@ -3,3 +3,10 @@
 def extract(raw_list):
     # Extract only complete records
     return [r for r in raw_list if "temp" in r and "city" in r]
+
+def transform(record):
+    # Clean one record
+    cleaned = {}
+    cleaned["city"] = record["city"].strip().title()
+    cleaned["temp"] = float(record["temp"])   # Might raise ValueError
+    return cleaned
